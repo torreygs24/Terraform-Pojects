92c9aa34449f5b2e4d8009a9abe5998929ac1eb8
torreygs24 <47759063+torreygs24@users.noreply.github.com>
2021-07-09 18:56:59 -0500

    Add files via upload

--- Data Stuctures/main.tf
+++ Data Stuctures/main.tf
@@ -1,11 +1,13 @@
 #there are three primitive types in Terraform: string, number, boolean
 variable "my_bool" {
-  type = bool
+  type    = bool
   default = true
 } # In the above example, you can sub the type for any listed type.
 # Primitive types have a singe value.
 
 # Collection and Structural types have multiple values.
+# Collection types: List, SET, MAP.
+# Structural Types: Object and Tuple
 # These types are LIST,SET, MAP.
 # LIST -List of values in sequence starting at position 0.
 # MAP - Collection of values identified by unique key of type string.
@@ -16,22 +18,19 @@ variable "my_list" {
 }
 
 variable "my_map" {
-  type = map(number) # A map with number values
+  type = map(number)
+  default = {
+    "statement"  = "Lupe Fiasco is the GOAT"
+    "statement1" = "No, JayZ is the GOAT"
+    "Statement3" = [2, 1, 2]
+  } # A map with number values
 }
-# list examples
-[1,2,3,4] # A valid list of 4 numbers with 1 position being 0
-["a", 2, fales] # Not valid list
+#["a", 2, false] # Not valid list
 
 # Map Examples
-{ # A map with two keys and list values
-        "one" = [3,1,4]
-        "two" = ["pi", "is", "good"]
-}
+# A map with two keys and list values
+#"one" = [3,1,4]
+#"two" = ["pi", "is", "good"]
 
-{ #A map with two keys and number values
-        "one" = 1
-        "two" = 2
-}
-var.map_var["one"] #retruens the value with the key "one"
 
 
