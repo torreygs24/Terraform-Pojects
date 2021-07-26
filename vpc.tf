96f83c44eee4df9bcec51e68e81e681bc13f7945
torreygs24 <47759063+torreygs24@users.noreply.github.com>
2021-07-26 12:09:26 -0500

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-# 1. Create vpc
-resource "aws_vpc" "prod_vpc" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
-  tags = {
-    Name        = "Production"
-    Environment = terraform.workspace
-  }
-}
