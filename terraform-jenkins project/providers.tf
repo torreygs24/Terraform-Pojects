34985e3254014eacbea9139bf444b75bc8db5017
torreygs24 <47759063+torreygs24@users.noreply.github.com>
2021-08-01 11:03:39 -0500

    Add files via upload

--- terraform-jenkins project/providers.tf
+++ terraform-jenkins project/providers.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 terraform {
   backend "s3" {
-    bucket = "javahome-tf-1212"
+    bucket = "jenkins-terraform214"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
